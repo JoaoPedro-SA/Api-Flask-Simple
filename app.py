161ad0c0d43fd4e8108b820c9afadcb4ed19b50f
João Pedro Silva Antunes <antunesjoaopedro3@gmail.com>
2025-08-19 20:40:33 -0300

    adicionando dados + atividade.txt

--- app.py
+++ app.py
@@ -1,5 +1,3 @@
-import os
-import sys 
 from controle.controle import *
 import flask
 
--- models/model.py
+++ models/model.py
@@ -1,2 +1,18 @@
+
+dados = { "usuarios": 
+     {
+          {id: 1, "nome": "Antônio", "idade": 30},
+          {id: 2, "nome": "Maria", "idade": 25},
+          {id: 3, "nome": "João", "idade": 28},
+          {id: 4, "nome": "Ana", "idade": 22},
+          {id: 5, "nome": "Pedro", "idade": 35},
+          {id: 6, "nome": "Lucas", "idade": 27},
+          {id: 7, "nome": "Carla", "idade": 29},
+          {id: 8, "nome": "Fernanda", "idade": 31},
+          {id: 9, "nome": "Roberto", "idade": 33},
+          {id: 10, "nome": "Juliana", "idade": 26}            
+     }
+     }
+
 def teste():
     return {"message": "Hello from the model!"} 
