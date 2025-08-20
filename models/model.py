8a603d315888e1edb2b1a1371b4e8c272af22155
João Pedro Silva Antunes <antunesjoaopedro3@gmail.com>
2025-08-19 21:41:06 -0300

    trocando os dados

--- models/model.py
+++ models/model.py
@@ -1,18 +1,18 @@
+dados = { 
+    "usuarios": [
+        {"id": 1, "nome": "Antônio", "email": "antonio@email.com"},
+        {"id": 2, "nome": "Maria", "email": "maria@email.com"},
+        {"id": 3, "nome": "João", "email": "joao@email.com"},
+        {"id": 4, "nome": "Ana", "email": "ana@email.com"},
+        {"id": 5, "nome": "Pedro", "email": "pedro@email.com"},
+        {"id": 6, "nome": "Lucas", "email": "lucas@email.com"},
+        {"id": 7, "nome": "Carla", "email": "carla@email.com"},
+        {"id": 8, "nome": "Fernanda", "email": "fernanda@email.com"},
+        {"id": 9, "nome": "Roberto", "email": "roberto@email.com"},
+        {"id": 10, "nome": "Juliana", "email": "juliana@email.com"}
+    ]
+}
 
-dados = { "usuarios": 
-     {
-          {id: 1, "nome": "Antônio", "idade": 30},
-          {id: 2, "nome": "Maria", "idade": 25},
-          {id: 3, "nome": "João", "idade": 28},
-          {id: 4, "nome": "Ana", "idade": 22},
-          {id: 5, "nome": "Pedro", "idade": 35},
-          {id: 6, "nome": "Lucas", "idade": 27},
-          {id: 7, "nome": "Carla", "idade": 29},
-          {id: 8, "nome": "Fernanda", "idade": 31},
-          {id: 9, "nome": "Roberto", "idade": 33},
-          {id: 10, "nome": "Juliana", "idade": 26}            
-     }
-     }
 
 def teste():
     return {"message": "Hello from the model!"} 
