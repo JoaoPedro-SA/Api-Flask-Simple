c9147680e891bae0697fae2ab535794b42bcf7a8
Alan Araujo da Silveira <2401855@salas.aulas>
2025-09-09 11:50:22 -0300

    Criação do dockerfile

--- app.py
+++ app.py
@@ -20,4 +20,4 @@ from swagger.swaggerconfig import configure_swagger
 configure_swagger(app)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=True)
